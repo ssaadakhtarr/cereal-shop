c677a42a62fd55080fea5590929ca7fa0ca9076b
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2023-06-30 06:03:01 -0400

    minor fixes

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f web_cereal
 docker build -t web_cereal .
-docker run --network=host --name=web_cereal --rm -p 5004:5004 -it web_cereal
+docker run --network=host --name=web_cereal --rm -p 5000:5000 -it web_cereal
